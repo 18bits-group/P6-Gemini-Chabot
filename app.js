e789b7fa027a14c600404d44db9725beceeaff96
Alessandra Santana <alesy2007@gmail.com>
2026-09-01 21:44:14 -0300

    Replace agentGemini import with server import

--- app.js
+++ app.js
@@ -1,3 +1,1 @@
-import agentGemini from "./src/server/agents/gemini.js";
-const msg = "Gemini da um salve pra galera do grupo ai?";
-console.log(await agentGemini(msg));
+import "./src/server/server.js";
